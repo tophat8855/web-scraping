35262066047cbedf04ba0a30e5fb7ad53a647908
tophat8855 <hrfarley@gmail.com>
2015-01-09 10:52:48 -0800

    star count completed

--- app.rb
+++ app.rb
@@ -2,15 +2,29 @@ require 'Nokogiri'
 require_relative './lib/review'
 
 def score_distribution(reviews)
-  
+
 end
 
 file = File.open('data/fat-angel.html')
 noko_html_doc = Nokogiri::HTML(file.read)
 
-array_of_reviews = noko_html_doc.css("ul.reviews > li").map {|review| Review.new(review)}
+array_of_reviews = []
 
+stars = {
+  "1.0" => 0,
+  "2.0" => 0,
+  "3.0" => 0,
+  "4.0" => 0,
+  "5.0" => 0,
+}
 
+noko_html_doc.css("ul.reviews > li").each do |review|
+  new_review = Review.new(review)
+  array_of_reviews << new_review
+  stars[new_review.star_count] += 1
+end
+
+p stars
 
 first_review = array_of_reviews.first
 p first_review.date
--- lib/review.rb
+++ lib/review.rb
@@ -2,9 +2,7 @@ class Review
   attr_accessor :content, :date, :star_count, :author, :text, :html
   def initialize(content)
     @content = content
-
     @date = @content.css('meta[itemprop="datePublished"]').attr("content").text
-
     @star_count = @content.css('meta[itemprop="ratingValue"]').attr("content").text
     @author = @content.css('li.user-name').text.strip
     @text = @content.css(".review-content p").text
